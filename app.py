6bc67e7640840aa24d3df0a802489744eec73a60
ClaudioLutz <claudio.lan@hotmail.com>
2021-11-07 19:36:15 +0100

    Statistik Löschungen und Neueintragungen hinzuzgefügt

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import os
 import pickle
-import numpy as np
 
 def daterange(start_date, end_date):
     dates=[]
@@ -28,11 +27,12 @@ def element_text(element):
 
 given_date = datetime.today().date() 
 end_date = given_date.replace(day=1)
-start_date = date(2020, 1, 1)
+start_date = date(2019, 1, 1)
 df=None
 data = []
+picklevar = "./dummy.pkl"
 
-if os.path.isfile("./dummy.pkl") == True:
+if os.path.isfile(picklevar) == True:
     df = pd.read_pickle("./dummy.pkl")
     df['date']= pd.to_datetime(df['date']).dt.date
     if df.date.min() <= (start_date + timedelta(days=3)) and df.date.max() >= (end_date - timedelta(days=3)):
@@ -75,6 +75,29 @@ if os.path.isfile("./dummy.pkl") == True:
             
         df = pd.DataFrame(data)
         df = df[(df["subrubric"] == "HR01") | (df["subrubric"] == "HR03")]
-        df.to_pickle("./dummy.pkl")
-df
+        df.to_pickle(picklevar)
+
+HR03 = df[df["subrubric"] == 'HR03']
+HR03 = HR03[['date','subrubric','kanton']]
+HR03['date'] = pd.to_datetime(df['date'])
+HR03_Count = HR03.groupby(pd.Grouper(key='date', freq='1M')).count()
+HR03_Count.rename({'subrubric': 'HR03'}, axis=1, inplace=True)
+HR01 = df[df["subrubric"] == 'HR01']
+HR01 = HR01[['date','subrubric','kanton']]
+HR01['date'] = pd.to_datetime(df['date'])
+HR01_Count = HR01.groupby(pd.Grouper(key='date', freq='1M')).count()
+HR01_Count.rename({'subrubric': 'HR01'}, axis=1, inplace=True)
+
+HR01_03 = pd.merge(HR01_Count, HR03_Count, on="date")
+
+fig_dims = (20,6)
+fig, ax = plt.subplots(figsize=fig_dims)
+sns.lineplot(data=HR01_03, x="date", y="HR03",ax=ax)
+sns.lineplot(data=HR01_03, x="date", y="HR01",ax=ax)
+plt.legend(labels=["Löschungen","Neueintragungen"])
+ax.plot()
+
+
+
+
 # %%
